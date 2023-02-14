581aeef9e77d0ca98f06721d25d4fd042a7bf3b2
Göktay İncekara <goktayincekara@gmail.com>
2023-02-14 11:15:43 +0300

    Webpack configurations and client-side setup

---
+++ Client/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default MainPage;
