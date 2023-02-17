deb6077a67aeb38714a628b269f3d36924359430
Göktay İncekara <goktayincekara@gmail.com>
2023-02-17 10:31:07 +0300

    User models are modified

--- Server/models/student.js
+++ Server/models/student.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const studentSchema = mongoose.Schema({
-  name: String,
+  fullname: String,
   birthday: Date,
-  schoolname: String,
-  year: String,
   email: { type: String, required: true },
   city: { type: String, required: true },
   phone: { type: Number, required: true },
