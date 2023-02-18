11110f2b5683581384cffcd7c9451ad23a15c648
Göktay İncekara <goktayincekara@gmail.com>
2023-02-18 22:40:15 +0300

    Card improvements

--- Client/src/components/Card/Card.js
+++ Client/src/components/Card/Card.js
@@ -2,20 +2,32 @@ import React from "react";
 import styles from "./customCard.module.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function CustomCard({ img, name, email, city, instructors, phone, date }) {
   return (
-    <Card className={styles.card}>
+    <Card className={`${styles.card} text-center`}>
       <Card.Img className={styles.card_img_top} variant="top" src={img} />
+      <Card.ImgOverlay>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+
+      <Card.Header as="h1">{name}</Card.Header>
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{email}</Card.Text>
-        <h1>{city}</h1>
-        <h1>{instructors}</h1>
-        <h1>{phone}</h1>
-        <h1>{date}</h1>
-        <Button variant="primary">Bize ulaşın!</Button>
+        <Card.Title>{email}</Card.Title>
+        <Card.Subtitle>{city}</Card.Subtitle>
+        <Card.Text>{phone}</Card.Text>
       </Card.Body>
+      <ListGroup variant="flush">
+        <ListGroup.Item>{instructors}</ListGroup.Item>
+      </ListGroup>
+      <Card.Footer className="text-muted">{date}</Card.Footer>
+      <Button variant="primary">Bize ulaşın!</Button>
     </Card>
   );
 }
