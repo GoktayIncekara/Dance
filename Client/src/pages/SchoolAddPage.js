c6c0a85e05aeb3cda0ca6d7c6fdbf9eb4d254369
Göktay İncekara <goktayincekara@gmail.com>
2023-02-18 10:42:57 +0300

    Add the school add section authentication

--- Client/src/pages/SchoolAddPage.js
+++ Client/src/pages/SchoolAddPage.js
@@ -2,10 +2,11 @@ import React, { useState, useContext } from "react";
 import FormInput from "../components/FormInput/FormInput";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth-context";
+import { useAuthenticate } from "../helpers/useAuthenticate";
+import { Link } from "react-router-dom";
 
 function SchoolAdd() {
-  const ctx = useContext(AuthContext);
+  const { ctx } = useAuthenticate();
   const API = axios.create({ baseURL: "http://localhost:5000" });
   const navigate = useNavigate();
 
@@ -32,68 +33,85 @@ function SchoolAdd() {
   };
 
   return (
-    <div className="form-container-schools-add">
-      <form onSubmit={handleSubmit}>
-        <h1 className="form-heading">Okul Ekle</h1>
-        <FormInput
-          name="schoolname"
-          type="text"
-          placeholder="Okul adı"
-          value={values["schoolname"]}
-          onChange={onChange}
-          errorMessage="Lütfen bir okul adı girin."
-          required
-        />
-        <FormInput
-          name="year"
-          type="text"
-          placeholder="Kuruluş Yılı"
-          value={values["year"]}
-          onChange={onChange}
-          errorMessage="Lütfen kuruluş yılınızı girin."
-          required
-          disabled
-        />
-        <FormInput
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={values["email"]}
-          onChange={onChange}
-          errorMessage="Lütfen geçerli bir email girin."
-          required
-        />
-        <FormInput
-          name="city"
-          type="text"
-          placeholder="Şehir"
-          value={values["city"]}
-          onChange={onChange}
-          errorMessage="Lütfen okulun bulunduğu şehri girin."
-          required
-        />
-        <FormInput
-          name="phone"
-          type="text"
-          placeholder="Telefon"
-          value={values["phone"]}
-          onChange={onChange}
-          errorMessage="Lütfen geçerli bir telefon numarası girin."
-          pattern="0[0-9]{10}"
-          required
-        />
-        <FormInput
-          name="instructors"
-          type="number"
-          placeholder="Eğitmen sayısı"
-          value={values["instructors"]}
-          onChange={onChange}
-          errorMessage="Lütfen eğitmen sayınızı girin."
-          min="0"
-          required
-        />
-        <button className="form-button">Okul kaydet!</button>
-      </form>
+    <div>
+      {ctx.user ? (
+        <div className="form-container-schools-add">
+          <form onSubmit={handleSubmit}>
+            <h1 className="form-heading">Okul Ekle</h1>
+            <FormInput
+              name="schoolname"
+              type="text"
+              placeholder="Okul adı"
+              value={values["schoolname"]}
+              onChange={onChange}
+              errorMessage="Lütfen bir okul adı girin."
+              required
+            />
+            <FormInput
+              name="year"
+              type="text"
+              placeholder="Kuruluş Yılı"
+              value={values["year"]}
+              onChange={onChange}
+              errorMessage="Lütfen kuruluş yılınızı girin."
+              required
+              disabled
+            />
+            <FormInput
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={values["email"]}
+              onChange={onChange}
+              errorMessage="Lütfen geçerli bir email girin."
+              required
+            />
+            <FormInput
+              name="city"
+              type="text"
+              placeholder="Şehir"
+              value={values["city"]}
+              onChange={onChange}
+              errorMessage="Lütfen okulun bulunduğu şehri girin."
+              required
+            />
+            <FormInput
+              name="phone"
+              type="text"
+              placeholder="Telefon"
+              value={values["phone"]}
+              onChange={onChange}
+              errorMessage="Lütfen geçerli bir telefon numarası girin."
+              pattern="0[0-9]{10}"
+              required
+            />
+            <FormInput
+              name="instructors"
+              type="number"
+              placeholder="Eğitmen sayısı"
+              value={values["instructors"]}
+              onChange={onChange}
+              errorMessage="Lütfen eğitmen sayınızı girin."
+              min="0"
+              required
+            />
+            <button className="form-button">Okul kaydet!</button>
+          </form>
+        </div>
+      ) : (
+        <div className="add-school-error">
+          <h2>
+            Okulunuzu ekleyebilmek için lütfen
+            <Link className="add-school-error-link" to="/register">
+              <h2 className="add-school-error-text"> Üye olun</h2>
+            </Link>
+            veya
+            <Link className="add-school-error-link" to="/login">
+              <h2 className="add-school-error-text"> Giriş yapın</h2>
+            </Link>
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
